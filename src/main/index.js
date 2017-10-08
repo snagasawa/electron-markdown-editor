01449711fe3f6fbb21c88c70e908ec7b6d3a1690
snagasawa <whitecrane8920@gmail.com>
2017-10-08 17:59:32 +0900

    create MainWindow

---
+++ src/main/index.js
@@ -0,0 +1,20 @@
+import { app } from 'electron'
+import createMainWindow from './createMainWindow'
+
+let mainWindow = null
+
+app.on('ready', () => {
+  mainWindow = createMainWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', (_e, hasVisibleWindows) => {
+  if (!hasVisibleWindows) {
+    mainWindow = createMainWindow()
+  }
+})
---
+++ src/renderer/app.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+render(<div>Hello, Electron and JSX</div>, document.getElementById('app'))
